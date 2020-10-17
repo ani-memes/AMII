77b2549482a00f3402010ef82e88e4b5b0a4c4d5
DATBOI <alexsimons9999@gmail.com>
2020-10-17 09:05:25 -0500

    Added fancy fade in animation

--- src/main/kotlin/io/unthrottled/amii/notifications/NotificationFactory.kt
+++ src/main/kotlin/io/unthrottled/amii/notifications/NotificationFactory.kt
@@ -1,12 +1,18 @@
 package io.unthrottled.amii.notifications
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.ui.ColorUtil
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.jcef.HwFacadeJPanel
+import com.intellij.util.ui.Animator
 import com.intellij.util.ui.UIUtil
 import io.unthrottled.amii.tools.BalloonTools.fetchBalloonParameters
+import java.awt.AlphaComposite
+import java.awt.Graphics
+import java.awt.Graphics2D
 import javax.swing.JLayeredPane
-import javax.swing.JPanel
 
 object NotificationFactory {
 
@@ -28,11 +34,54 @@ object NotificationFactory {
     )
     val rootPane = UIUtil.getRootPane(ideFrame.component)
     val layeredPane = rootPane?.layeredPane
-    val panel = JPanel()
+    val panel = MemePanel()
     panel.add(label)
     panel.size = label.preferredSize
 
     layeredPane?.add(panel)
     layeredPane?.setLayer(panel, JLayeredPane.POPUP_LAYER, 0)
+
+    runAnimation(layeredPane, panel)
+  }
+
+  const val TOTAL_FRAMES = 8
+  const val CYCLE_DURATION = 500
+
+  private fun runAnimation(layeredPane: JLayeredPane?, memePanel: MemePanel) {
+    val animator = object : Animator("Meme Machine", TOTAL_FRAMES, CYCLE_DURATION, false) {
+      override fun paintNow(frame: Int, totalFrames: Int, cycle: Int) {
+        memePanel.alpha = frame.toFloat() / totalFrames
+      }
+
+      override fun paintCycleEnd() {
+        if (isForward) {
+          memePanel.repaint()
+
+          // set fade out timeer
+        } else {
+          layeredPane?.remove(memePanel)
+          layeredPane?.revalidate()
+          layeredPane?.revalidate()
+        }
+        Disposer.dispose(this)
+      }
+    }
+
+    animator.resume()
+  }
+}
+
+class MemePanel : HwFacadeJPanel(), Disposable {
+
+  var alpha = 0.0f
+
+  override fun paintComponent(g: Graphics?) {
+    super.paintComponent(g)
+    if (g !is Graphics2D) return
+
+    g.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha)
+  }
+
+  override fun dispose() {
   }
 }
