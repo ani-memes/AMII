6670abe7665f52e7fa99aca636324b0161502053
DATBOI <alexsimons9999@gmail.com>
2020-11-28 08:41:34 -0600

    Added debouncer

---
+++ src/main/kotlin/io/unthrottled/amii/tools/Debouncer.kt
@@ -0,0 +1,64 @@
+package io.unthrottled.amii.tools
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.Disposer
+import com.intellij.util.Alarm
+import java.util.LinkedList
+
+fun interface Debouncer {
+  fun debounce(toDebounce: () -> Unit)
+}
+
+fun interface BufferedDebouncer<T> {
+  fun debounceAndBuffer(t: T, onDebounced: (List<T>) -> Unit)
+}
+
+class AlarmDebouncer<T>(
+  private val interval: Int,
+  parentDisposable: Disposable,
+) :
+  Debouncer,
+  BufferedDebouncer<T>,
+  Disposable {
+  private val alarm: Alarm = Alarm(this)
+
+  init {
+    Disposer.register(parentDisposable, this)
+  }
+
+  override fun debounce(toDebounce: () -> Unit) {
+    performDebounce({
+      alarm.addRequest(toDebounce, interval)
+    })
+  }
+
+  private val buffer = LinkedList<T>()
+
+  override fun debounceAndBuffer(t: T, onDebounced: (List<T>) -> Unit) {
+    performDebounce({
+      alarm.addRequest(
+        {
+          buffer.add(t)
+          onDebounced(buffer.toMutableList())
+          buffer.clear()
+        },
+        interval
+      )
+    }) {
+      buffer.push(t)
+    }
+  }
+
+  @Volatile
+  private var previousOnDebounce: (() -> Unit)? = null
+
+  private fun performDebounce(setupDebounce: () -> Unit, onDebounced: () -> Unit = {}) {
+    previousOnDebounce?.invoke()
+    previousOnDebounce = onDebounced
+    alarm.cancelAllRequests()
+    setupDebounce()
+  }
+
+  override fun dispose() {
+  }
+}
