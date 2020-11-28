767fd4b07ddfefc4530150450f2cfb9507c96ddf
DATBOI <alexsimons9999@gmail.com>
2020-11-28 08:20:50 -0600

    Refactored out process execution to separate listener

--- src/main/kotlin/io/unthrottled/amii/listeners/ExitCodeListener.kt
+++ src/main/kotlin/io/unthrottled/amii/listeners/ExitCodeListener.kt
@@ -1,22 +1,18 @@
 package io.unthrottled.amii.listeners
 
 import com.intellij.execution.ExecutionListener
-import com.intellij.execution.InputRedirectAware
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
-import com.intellij.util.messages.Topic
 import io.unthrottled.amii.config.Config
 import io.unthrottled.amii.config.Config.Companion.DEFAULT_DELIMITER
 import io.unthrottled.amii.config.ConfigListener
 import io.unthrottled.amii.config.ConfigListener.Companion.CONFIG_TOPIC
 import io.unthrottled.amii.events.EVENT_TOPIC
 import io.unthrottled.amii.events.UserEvent
-import io.unthrottled.amii.listeners.NamedProcesses.JUNIT
-import io.unthrottled.amii.listeners.NamedProcesses.OTHER
 import io.unthrottled.amii.services.ProcessHandlerService.wasCanceled
 
 const val OK_EXIT_CODE = 0
@@ -25,33 +21,6 @@ const val FORCE_KILLED_EXIT_CODE = 130
 fun String.toExitCodes(): Set<Int> = this.split(DEFAULT_DELIMITER)
   .filter { it.isNotEmpty() }
   .map { it.trim().toInt() }.toSet()
-
-fun ExecutionEnvironment.isJUnit(): Boolean {
-  val runProfile = this.runProfile
-  return runProfile is InputRedirectAware &&
-    runProfile.type.displayName.equals("JUnit", ignoreCase = true)
-}
-
-data class NamedProcess(
-  val type: NamedProcesses,
-  val id: Long,
-)
-
-enum class NamedProcesses {
-  JUNIT, OTHER,
-}
-
-interface ProcessLifecycleListener {
-
-  fun onProcessStarted(startedProcess: NamedProcess) {}
-
-  fun onProcessCompleted(completedProcess: NamedProcess) {}
-}
-
-val PROCESS_LIFECYCLE_TOPIC = Topic.create(
-  "Process Lifecycle",
-  ProcessLifecycleListener::class.java
-)
 
 class ExitCodeListener(private val project: Project) : ExecutionListener, Disposable {
   private val log = Logger.getInstance(javaClass)
@@ -73,34 +42,12 @@ class ExitCodeListener(private val project: Project) : ExecutionListener, Dispos
   override fun dispose() {
   }
 
-  override fun processStarting(executorId: String, env: ExecutionEnvironment) {
-    project.messageBus
-      .syncPublisher(PROCESS_LIFECYCLE_TOPIC)
-      .onProcessStarted(
-        NamedProcess(
-          if (env.isJUnit()) JUNIT
-          else OTHER,
-          env.executionId,
-        )
-      )
-  }
-
   override fun processTerminated(
     executorId: String,
     env: ExecutionEnvironment,
     handler: ProcessHandler,
     exitCode: Int
   ) {
-    project.messageBus
-      .syncPublisher(PROCESS_LIFECYCLE_TOPIC)
-      .onProcessCompleted(
-        NamedProcess(
-          if (env.isJUnit()) JUNIT
-          else OTHER,
-          env.executionId,
-        )
-      )
-
     log.debug("Observed exit code of $exitCode")
     if (wasCanceled(handler).not() &&
       allowedExitCodes.contains(exitCode).not() &&
---
+++ src/main/kotlin/io/unthrottled/amii/listeners/ProcessExecutionListener.kt
@@ -0,0 +1,67 @@
+package io.unthrottled.amii.listeners
+
+import com.intellij.execution.ExecutionListener
+import com.intellij.execution.InputRedirectAware
+import com.intellij.execution.process.ProcessHandler
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.project.Project
+import com.intellij.util.messages.Topic
+
+fun ExecutionEnvironment.isJUnit(): Boolean {
+  val runProfile = this.runProfile
+  return runProfile is InputRedirectAware &&
+    runProfile.type.displayName.equals("JUnit", ignoreCase = true)
+}
+
+data class NamedProcess(
+  val type: NamedProcesses,
+  val id: Long,
+)
+
+enum class NamedProcesses {
+  JUNIT, OTHER,
+}
+
+interface ProcessLifecycleListener {
+
+  fun onProcessStarted(startedProcess: NamedProcess) {}
+
+  fun onProcessCompleted(completedProcess: NamedProcess) {}
+}
+
+val PROCESS_LIFECYCLE_TOPIC = Topic.create(
+  "Process Lifecycle",
+  ProcessLifecycleListener::class.java
+)
+
+class ProcessExecutionListener(private val project: Project) : ExecutionListener {
+
+  override fun processStarting(executorId: String, env: ExecutionEnvironment) {
+    project.messageBus
+      .syncPublisher(PROCESS_LIFECYCLE_TOPIC)
+      .onProcessStarted(
+        NamedProcess(
+          if (env.isJUnit()) NamedProcesses.JUNIT
+          else NamedProcesses.OTHER,
+          env.executionId,
+        )
+      )
+  }
+
+  override fun processTerminated(
+    executorId: String,
+    env: ExecutionEnvironment,
+    handler: ProcessHandler,
+    exitCode: Int
+  ) {
+    project.messageBus
+      .syncPublisher(PROCESS_LIFECYCLE_TOPIC)
+      .onProcessCompleted(
+        NamedProcess(
+          if (env.isJUnit()) NamedProcesses.JUNIT
+          else NamedProcesses.OTHER,
+          env.executionId,
+        )
+      )
+  }
+}
