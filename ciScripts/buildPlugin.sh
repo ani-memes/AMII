8d9d078385a4a74b6b2ae179e2360c62e32bfabb
DATBOI <alexsimons9999@gmail.com>
2020-12-17 18:24:35 -0600

    Added CIScripts

---
+++ ciScripts/buildPlugin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pre-Build
+#git add .
+#git stash
+#git fetch
+#git checkout master
+#git pull origin master
+#./gradlew markdownToHtml
+
+# Build
+./gradlew clean buildPlugin
+#git add .
+#git stash
+#git stash clear
+#
