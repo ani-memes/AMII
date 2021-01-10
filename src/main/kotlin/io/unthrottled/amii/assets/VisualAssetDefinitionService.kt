62429294753dc40feeb59aa310fdf767695d5d15
Alex Simons <alexsimons9999@gmail.com>
2021-01-10 10:45:10 -0600

    Diversify Initial Experience (#34)
    * Attempting to download more assets for onboarding
    
    * Not downloading everything at once

--- src/main/kotlin/io/unthrottled/amii/assets/VisualAssetDefinitionService.kt
+++ src/main/kotlin/io/unthrottled/amii/assets/VisualAssetDefinitionService.kt
@@ -1,36 +1,19 @@
 package io.unthrottled.amii.assets
 
-import io.unthrottled.amii.services.ExecutionService
+import io.unthrottled.amii.assets.VisualEntitySupplier.getLocalAssetsByCategory
+import io.unthrottled.amii.assets.VisualEntitySupplier.getRemoteAssetsByCategory
 import io.unthrottled.amii.tools.Logging
 import io.unthrottled.amii.tools.toOptional
 import java.util.Optional
-import kotlin.random.Random
 
 object VisualAssetDefinitionService : Logging {
 
   private val remoteAssetManager = VisualContentManager
 
-  private val random = Random(System.currentTimeMillis())
-
   fun getRandomAssetByCategory(
     memeAssetCategory: MemeAssetCategory,
   ): Optional<VisualMemeContent> =
-    chooseRandomAsset(memeAssetCategory)
-
-  private fun chooseRandomAsset(
-    memeAssetCategory: MemeAssetCategory,
-  ) =
-    chooseAssetAtRandom(
-      VisualEntityService.instance.supplyPreferredLocalAssetDefinitions()
-        .filterByCategory(memeAssetCategory)
-        .ifEmpty {
-          VisualEntityService.instance.supplyPreferredGenderLocalAssetDefinitions()
-            .filterByCategory(memeAssetCategory)
-        }.ifEmpty {
-          VisualEntityService.instance.supplyAllLocalAssetDefinitions()
-            .filterByCategory(memeAssetCategory)
-        }
-    )
+    chooseAssetAtRandom(getLocalAssetsByCategory(memeAssetCategory))
       .map {
         resolveAsset(memeAssetCategory, it.representation)
       }.orElseGet {
@@ -41,45 +24,27 @@ object VisualAssetDefinitionService : Logging {
     memeAssetCategory: MemeAssetCategory,
     assetDefinition: VisualAssetRepresentation,
   ): Optional<VisualMemeContent> {
-    downloadNewAsset(memeAssetCategory)
+    BackgroundAssetService.downloadNewAssets(memeAssetCategory)
     return remoteAssetManager.resolveAsset(assetDefinition)
-  }
-
-  private fun downloadNewAsset(
-    memeAssetCategory: MemeAssetCategory,
-  ) {
-    ExecutionService.executeAsynchronously {
-      fetchRemoteAsset(memeAssetCategory)
-        .map { it.audioId }
-        .ifPresent {
-          AudibleAssetDefinitionService.getAssetById(it)
-        }
-    }
   }
 
   private fun fetchRemoteAsset(
     memeAssetCategory: MemeAssetCategory,
-  ): Optional<VisualMemeContent> =
-    chooseAssetAtRandom(
-      VisualEntityService.instance.supplyPreferredRemoteAssetDefinitions()
-        .filterByCategory(memeAssetCategory)
-        .ifEmpty {
-          VisualEntityService.instance.supplyPreferredGenderRemoteAssetDefinitions()
-            .filterByCategory(memeAssetCategory)
-        }.ifEmpty {
-          VisualEntityService.instance.supplyAllRemoteAssetDefinitions()
-            .filterByCategory(memeAssetCategory)
-        }
+  ): Optional<VisualMemeContent> {
+    BackgroundAssetService.downloadNewAssets(memeAssetCategory)
+    return chooseAssetAtRandom(
+      getRemoteAssetsByCategory(memeAssetCategory)
     ).flatMap { remoteAssetManager.resolveAsset(it.representation) }
-
-  private fun chooseAssetAtRandom(
-    assetDefinitions: Collection<VisualAssetEntity>
-  ): Optional<VisualAssetEntity> =
-    assetDefinitions
-      .toOptional()
-      .filter { it.isNotEmpty() }
-      .flatMap { VisualAssetProbabilityService.instance.pickAssetFromList(it) }
+  }
 }
+
+fun chooseAssetAtRandom(
+  assetDefinitions: Collection<VisualAssetEntity>
+): Optional<VisualAssetEntity> =
+  assetDefinitions
+    .toOptional()
+    .filter { it.isNotEmpty() }
+    .flatMap { VisualAssetProbabilityService.instance.pickAssetFromList(it) }
 
 fun Collection<VisualAssetEntity>.filterByCategory(
   category: MemeAssetCategory
---
+++ src/main/kotlin/io/unthrottled/amii/assets/VisualEntitySupplier.kt
@@ -0,0 +1,26 @@
+package io.unthrottled.amii.assets
+
+object VisualEntitySupplier {
+
+  fun getLocalAssetsByCategory(memeAssetCategory: MemeAssetCategory) =
+    VisualEntityService.instance.supplyPreferredLocalAssetDefinitions()
+      .filterByCategory(memeAssetCategory)
+      .ifEmpty {
+        VisualEntityService.instance.supplyPreferredGenderLocalAssetDefinitions()
+          .filterByCategory(memeAssetCategory)
+      }.ifEmpty {
+        VisualEntityService.instance.supplyAllLocalAssetDefinitions()
+          .filterByCategory(memeAssetCategory)
+      }
+
+  fun getRemoteAssetsByCategory(memeAssetCategory: MemeAssetCategory) =
+    VisualEntityService.instance.supplyPreferredRemoteAssetDefinitions()
+      .filterByCategory(memeAssetCategory)
+      .ifEmpty {
+        VisualEntityService.instance.supplyPreferredGenderRemoteAssetDefinitions()
+          .filterByCategory(memeAssetCategory)
+      }.ifEmpty {
+        VisualEntityService.instance.supplyAllRemoteAssetDefinitions()
+          .filterByCategory(memeAssetCategory)
+      }
+}
