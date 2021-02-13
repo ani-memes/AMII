fcc237f593783947cc5896150cbee5d97827669b
DATBOI <alexsimons9999@gmail.com>
2021-02-13 07:54:49 -0600

    Actually fixing lint, thanks Detekt...

--- src/main/kotlin/io/unthrottled/amii/core/personality/emotions/NegativeEmotionDerivationUnit.kt
+++ src/main/kotlin/io/unthrottled/amii/core/personality/emotions/NegativeEmotionDerivationUnit.kt
@@ -53,8 +53,7 @@ internal class NegativeEmotionDerivationUnit(
     val observedNegativeEvents = emotionalState.observedNegativeEvents
     val cooledDownNegativeEvents = max(0, observedNegativeEvents - 1)
     return emotionalState.copy(
-      mood =
-      if (hasCalmedDown(cooledDownNegativeEvents)) Mood.CALM
+      mood = if (hasCalmedDown(cooledDownNegativeEvents)) Mood.CALM
       else pickNegativeMood(cooledDownNegativeEvents),
       observedNegativeEvents = cooledDownNegativeEvents
     )
