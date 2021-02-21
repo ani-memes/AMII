88a543d8542645be79d24dfdac65dc778bf2ab09
DATBOI <alexsimons9999@gmail.com>
2021-02-21 09:01:33 -0600

    Added notification for action

--- src/main/kotlin/io/unthrottled/amii/actions/MinimalModeAction.kt
+++ src/main/kotlin/io/unthrottled/amii/actions/MinimalModeAction.kt
@@ -6,6 +6,8 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAware
 import io.unthrottled.amii.config.Config
 import io.unthrottled.amii.config.ConfigListener
+import io.unthrottled.amii.onboarding.UpdateNotification
+import io.unthrottled.amii.tools.PluginMessageBundle
 
 class MinimalModeAction : ToggleAction(), DumbAware {
   override fun isSelected(e: AnActionEvent): Boolean =
@@ -16,5 +18,18 @@ class MinimalModeAction : ToggleAction(), DumbAware {
     ApplicationManager.getApplication().messageBus.syncPublisher(
       ConfigListener.CONFIG_TOPIC
     ).pluginConfigUpdated(Config.instance)
+    if (state) {
+      UpdateNotification.sendMessage(
+        PluginMessageBundle.message("actions.minimal.enabled.title"),
+        PluginMessageBundle.message("actions.minimal.enabled.message"),
+        e.project
+      )
+    } else {
+      UpdateNotification.sendMessage(
+        PluginMessageBundle.message("actions.minimal.disabled.title"),
+        PluginMessageBundle.message("actions.minimal.disabled.message"),
+        e.project
+      )
+    }
   }
 }
