0fbccbc2ed42f83bbf74d49bece1db6c4888c9fe
DATBOI <alexsimons9999@gmail.com>
2021-02-23 17:43:36 -0600

    Fixed issue with sound not playing on Linux machines

--- src/main/kotlin/io/unthrottled/amii/memes/player/ClipSoundPlayer.kt
+++ src/main/kotlin/io/unthrottled/amii/memes/player/ClipSoundPlayer.kt
@@ -2,8 +2,11 @@ package io.unthrottled.amii.memes.player
 
 import io.unthrottled.amii.assets.AudibleContent
 import io.unthrottled.amii.config.Config
+import io.unthrottled.amii.tools.runSafelyWithResult
+import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.Clip
+import javax.sound.sampled.DataLine
 import javax.sound.sampled.FloatControl
 import javax.sound.sampled.LineEvent
 import kotlin.math.log10
@@ -17,32 +20,48 @@ class ClipSoundPlayer(
     private const val DECIBEL_MULTIPLICAND = 20F
   }
 
-  private val clip: Clip =
+  @Suppress("MagicNumber")
+  private val clip: Clip? =
     AudioSystem.getAudioInputStream(audibleAssetContent.filePath.toURL())
       .use { inputStream ->
-        val newClip = AudioSystem.getClip()
-        newClip.open(inputStream)
-        newClip.addLineListener {
-          if (it.type == LineEvent.Type.STOP) {
-            newClip.close()
+        runSafelyWithResult({
+          val newClip = AudioSystem.getClip()
+          newClip.open(inputStream)
+          newClip.addLineListener {
+            if (it.type == LineEvent.Type.STOP) {
+              newClip.close()
+            }
+          }
+          newClip
+        }) {
+          runSafelyWithResult({
+            val format = AudioFormat(
+              AudioFormat.Encoding.PCM_SIGNED,
+              44100F,
+              16, 2, 4,
+              AudioSystem.NOT_SPECIFIED.toFloat(), true
+            )
+            val info = DataLine.Info(Clip::class.java, format)
+            AudioSystem.getLine(info) as Clip
+          }) {
+            null
           }
         }
-        newClip
       }
 
   init {
-    val gainControl = clip.getControl(FloatControl.Type.MASTER_GAIN) as FloatControl
+    val gainControl = clip?.getControl(FloatControl.Type.MASTER_GAIN) as FloatControl
     gainControl.value = DECIBEL_MULTIPLICAND * log10(Config.instance.volume)
   }
 
   override val duration: Long
-    get() = clip.microsecondLength / MILLS_DIVISOR
+    get() = clip?.microsecondLength?.div(MILLS_DIVISOR) ?: -1L
 
   override fun play() {
-    clip.start()
+    clip?.start()
   }
 
   override fun stop() {
-    clip.close()
+    clip?.close()
   }
 }
