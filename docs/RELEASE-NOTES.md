d226fe6f9ac5a97ecceb9262209fedb2dcfd4aff
DATBOI <alexsimons9999@gmail.com>
2021-04-12 11:59:40 -0500

    Updated changelog and bumped version to v0.9.2

--- docs/RELEASE-NOTES.md
+++ docs/RELEASE-NOTES.md
@@ -1,15 +1,5 @@
 ### Added
-- MIKU now has the ability to react positively to exit codes that you choose.
-  [See the updated documentation for more details.](https://github.com/ani-memes/AMII#exit-codes)
+- Suggestion of the [Anime Memes - Rider Extension](https://github.com/ani-memes/amii-rider-extension), for AMII's Rider Users.
 
 ### Fixed
-- MIKU attempting to react on a project that is disposed (again...).
-- Actually addressing the sound playback errors on Linux
-- Attempting to cool down a MIKU from a disposed project.
-- MIKU not paying attention to the relax action.
-- Correctly initializing settings when using the `Search Everywhere` action.
-- The name of the mood status bar icon.
-
-
-### Changed
-- Memes with sound only play as long as the sound clip (when sound is enabled).
+- Plugin not working when the IDE is configured to use an HTTP proxy.
--- src/main/kotlin/io/unthrottled/amii/promotion/MemePromotionDialog.kt
+++ src/main/kotlin/io/unthrottled/amii/promotion/MemePromotionDialog.kt
@@ -10,7 +10,6 @@ import com.intellij.util.ui.UIUtil
 import icons.AMIIIcons
 import io.unthrottled.amii.assets.AssetCategory
 import io.unthrottled.amii.assets.ContentAssetManager
-import io.unthrottled.amii.assets.ContentAssetManager.FALLBACK_ASSET_SOURCE
 import io.unthrottled.amii.assets.ContentAssetManager.assetSource
 import io.unthrottled.amii.config.Constants
 import io.unthrottled.amii.tools.PluginMessageBundle
@@ -31,23 +30,16 @@ class PromotionAssets(
 ) {
 
   val pluginLogoURL: String
-  val promotionAssetURL: String
 
   init {
     pluginLogoURL = getPluginLogo()
-    promotionAssetURL = getPromotionAsset()
   }
 
   private fun getPluginLogo(): String = ContentAssetManager.resolveAssetUrl(
     AssetCategory.PROMOTION,
-    "amii/logo.png"
+    "amii/amii_rider_extension_logo.png",
   ).map { it.toString() }
-    .orElse("$assetSource/promotion/amii/logo.png")
-
-  private fun getPromotionAsset(): String =
-    ContentAssetManager.resolveAssetUrl(AssetCategory.PROMOTION, "motivator/promotion.gif")
-      .map { it.toString() }
-      .orElse("$FALLBACK_ASSET_SOURCE/promotion/motivator/promotion.gif")
+    .orElse("$assetSource/promotion/amii/amii_rider_extension_logo.png")
 }
 
 @Suppress("MaxLineLength")
@@ -78,6 +70,7 @@ class AniMemePromotionDialog(
         )
       }
     )
+
     init()
   }
 
@@ -181,10 +174,10 @@ class AniMemePromotionDialog(
                 text-align: center;
               }
           </style>
-          <title>Motivator</title>
       </head>
       <body>
-      <div class='logo-container'><img src="$pluginLogoURL" class='display-image' alt='Ani-Meme Plugin Logo'/>
+      <div class='logo-container'><img src="$pluginLogoURL" class='display-image'
+       style="max-height: 256px" alt='Ani-Meme Rider Extension Logo'/>
       </div>
       ${getPromotionContent(promotionAssets.isNewUser)}
       <br/>
@@ -206,25 +199,21 @@ class AniMemePromotionDialog(
   }
 
   private fun newUserPromotion(): String {
-    val promotionAssetURL = promotionAssets.promotionAssetURL
-
     @Language("HTML")
     val html =
       """
-            <h2 class='header'>Your new virtual companion!</h2>
+            <h2 class='header'>Extra Attention Required!</h2>
       <div style='margin: 8px 0 0 100px'>
         <p>
-          <a href='https://plugins.jetbrains.com/plugin/15865-amii'>The Anime Meme plugin</a>
-          gives your IDE more personality by using anime memes. <br/> You will get an assistant that will interact
-          with you as you build code.
-          <br/>Such as when your programs fail to run or tests pass/fail. Your companion<br/>
-          has the ability to react to these events. Which will most likely take the form <br/> of a reaction gif of
-          your favorite character(s)!
+          The Rider IDE is a special platform that requires extra love <br>
+          and attention to get AMII to work. <a href='https://plugins.jetbrains.com/plugin/16518-anime-memes--rider-extension'>The Anime Meme Ride Extension</a>
+          enables <br>
+          full functionality of the <a href='https://github.com/ani-memes/AMII'>Anime Meme plugin</a> on the Rider Platform.
+          Don't miss out <br/> on any of the important features supplied by AMII!
         </p>
       </div>
       <br/>
-      <h3 class='info-foreground'>Bring Anime Memes to your IDE today!</h3>
-      <div class='display-image'><img src='$promotionAssetURL' height="150"/></div>
+      <h3 class='info-foreground'>Get the complete experience!</h3>
       <br/>
       """.trimIndent()
     return html
@@ -234,28 +223,25 @@ class AniMemePromotionDialog(
     @Language("HTML")
     val html =
       """
-            <h2 class='header'>A brand new experience!</h2>
+            <h2 class='header'>You're Missing Out!</h2>
       <div style='margin: 8px 0 0 100px'>
         <p>
-          As of Waifu Motivator v2.0, notifications have been moved to
-<a href='https://plugins.jetbrains.com/plugin/15865-amii'>the Anime Meme</a> plugin. <br><br>
-          <div>Whats better?<br/>
+          I learned that the Rider IDE is a special platform that requires extra love <br>
+        and attention to get AMII to work. <a href='https://plugins.jetbrains.com/plugin/16518-anime-memes--rider-extension'>The Anime Meme Ride Extension</a>
+        enables <br>
+        full functionality of the <a href='https://plugins.jetbrains.com/plugin/15865-amii'>the Anime Meme</a> plugin. <br><br>
+          <div>What's provided by the extension<br/>
             <ul>
-                <li>More Content!</li>
-                <li>More Customization!</li>
-            </ul>
-          Breaking Changes:
-            <ul>
-                <li>Removed titled notifications.</li>
-                <li>Your previous configurations will be lost (Sorry!).</li>
+                <li>Unit Test Reactions.</li>
+                <li>Build Task Interactions.</li>
             </ul>
             </div>
-          For a list of more breaking changes and enhancements please see
-          <a href="https://github.com/waifu-motivator/waifu-motivator-plugin/blob/master/docs/CHANGELOG.md">the changelog</a>
+          For a list of more features provided and enhancements <br> please see
+          <a href="https://github.com/ani-memes/amii-rider-extension#features">the documentation.</a>
         </p>
       </div>
       <br/>
-      <h3 class='info-foreground'>I hope you enjoy!</h3>
+      <h3 class='info-foreground'>Get the complete experience!</h3>
       <br/>
       """.trimIndent()
     return html
