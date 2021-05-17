f5873444be839e6e22ff64175a1d9fd96eab9d06
DATBOI <alexsimons9999@gmail.com>
2021-05-17 06:11:58 -0500

    Start of the clear action

---
+++ src/main/kotlin/io/unthrottled/amii/actions/ClearAction.kt
@@ -0,0 +1,11 @@
+package io.unthrottled.amii.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+
+class ClearAction : AnAction(), DumbAware {
+  override fun actionPerformed(e: AnActionEvent) {
+
+  }
+}
