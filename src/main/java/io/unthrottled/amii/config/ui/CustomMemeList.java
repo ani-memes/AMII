970fa321074b450ddc21ecd045fa4255bd4a8404
Alex Simons <alexsimons9999@gmail.com>
2022-07-18 06:14:42 -0500

    First pass meme panel stuff.

--- src/main/java/io/unthrottled/amii/config/ui/CustomMemeList.java
+++ src/main/java/io/unthrottled/amii/config/ui/CustomMemeList.java
@@ -1,20 +1,27 @@
 package io.unthrottled.amii.config.ui;
 
+import com.intellij.uiDesigner.core.GridConstraints;
+
 import javax.swing.JButton;
-import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class CustomMemeList {
-  private JList list1;
   private JButton button1;
+  private JPanel rootPane;
+  private JScrollPane memesScroller;
+  private JPanel ayyLmao;
 
   public CustomMemeList() {
-
+    CustomMemePanel customMemePanel = new CustomMemePanel();
+    ayyLmao.add(customMemePanel.getComponent(), new GridConstraints());
   }
 
-
-
   public JPanel getComponent() {
-    return null;
+    return rootPane;
+  }
+
+  private void createUIComponents() {
+    // TODO: place custom component creation code here
   }
 }
--- src/main/java/io/unthrottled/amii/config/ui/CustomMemePanel.java
+++ src/main/java/io/unthrottled/amii/config/ui/CustomMemePanel.java
@@ -1,5 +1,8 @@
 package io.unthrottled.amii.config.ui;
 
+import com.intellij.ui.components.JBLabel;
+import org.intellij.lang.annotations.Language;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -12,9 +15,15 @@ public class CustomMemePanel {
   private JButton testMeme;
   private JPanel visualAssetDisplay;
   private JLabel assetDescription;
+  private JPanel rootPane;
+  private JBLabel memeDisplay;
 
+  public CustomMemePanel() {
+    @Language("HTML") String meme = "<html><img src=\"file:///Users/alexsimons/Downloads/zero-two-and-hiro-anime.gif\" /></html>";
+    memeDisplay.setText(meme);
+  }
 
   public JPanel getComponent() {
-    return null;
+    return rootPane;
   }
 }
