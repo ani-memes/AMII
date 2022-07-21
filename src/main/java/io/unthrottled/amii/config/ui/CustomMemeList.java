2d1e8d104c6bdf220ad0c876b135b9b88e822794
Alex Simons <alexsimons9999@gmail.com>
2022-07-21 06:05:17 -0500

    Able to select a directory meow.

--- src/main/java/io/unthrottled/amii/config/ui/CustomMemeList.java
+++ src/main/java/io/unthrottled/amii/config/ui/CustomMemeList.java
@@ -1,18 +1,29 @@
 package io.unthrottled.amii.config.ui;
 
+import com.intellij.execution.ExecutionBundle;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.ComponentWithBrowseButton;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.LabeledComponent;
+import com.intellij.openapi.ui.TextComponentAccessor;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.vfs.VirtualFile;
 import io.unthrottled.amii.assets.MemeAsset;
 import io.unthrottled.amii.assets.VisualAssetEntity;
 import io.unthrottled.amii.assets.VisualAssetRepresentation;
 import io.unthrottled.amii.config.ConfigSettingsModel;
 import io.unthrottled.amii.tools.AssetTools;
 import io.unthrottled.doki.settings.CustomStickerChooser;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,29 +32,45 @@ import java.util.Collections;
 import java.util.function.Consumer;
 
 public class CustomMemeList {
+  private Consumer<MemeAsset> onTest;
   private ConfigSettingsModel pluginSettingsModel;
-  private JButton chooseDirectoryButton;
   private JPanel rootPane;
   private JScrollPane memesScroller;
   private JPanel ayyLmao;
+  private JPanel selectDir;
 
   public CustomMemeList(
     Consumer<MemeAsset> onTest,
     ConfigSettingsModel pluginSettingsModel
   ) {
+    this.onTest = onTest;
     this.pluginSettingsModel = pluginSettingsModel;
     ayyLmao.setLayout(new BoxLayout(ayyLmao, BoxLayout.PAGE_AXIS));
+  }
+
+  private void populateDirectory(String workingDirectory) {
+    if(workingDirectory.isBlank()) {
+      return;
+    }
+
+    for (int i = 0; i < ayyLmao.getComponentCount(); i++) {
+      try {
+        ayyLmao.remove(0);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
     try {
       Files.walk(
-        Paths.get("/Users/alexsimons/Downloads/customAssets")
+        Paths.get(workingDirectory)
       )
         .filter(Files::isReadable)
         .filter(Files::isRegularFile)
-//        .filter(path -> path.endsWith(".gif"))
         .forEach(path -> {
           String id = AssetTools.calculateMD5Hash(path);
           CustomMemePanel customMemePanel = new CustomMemePanel(
-            onTest,
+            this.onTest,
             new VisualAssetEntity(
               id,
               path.toUri().toString(),
@@ -67,26 +94,11 @@ public class CustomMemeList {
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-    chooseDirectoryButton.addActionListener(e -> {
-      CustomStickerChooser dialog = new CustomStickerChooser(
-        Arrays.stream(ProjectManager.getInstance().getOpenProjects()).findFirst().orElse(
-          ProjectManager.getInstance().getDefaultProject()
-        ),
-        this.pluginSettingsModel.getCustomAssetsPath()
-      );
-
-      dialog.showAndGet();
-
-      if (dialog.getExitCode() == DialogWrapper.OK_EXIT_CODE) {
-        String path = dialog.getPath();
-        this.pluginSettingsModel.setCustomAssetsPath(path);
-      }
-    });
   }
 
   public void setPluginSettingsModel(ConfigSettingsModel pluginSettingsModel) {
     this.pluginSettingsModel = pluginSettingsModel;
+    populateDirectory(pluginSettingsModel.getCustomAssetsPath());
   }
 
   public JPanel getComponent() {
@@ -94,6 +106,24 @@ public class CustomMemeList {
   }
 
   private void createUIComponents() {
-    // TODO: place custom component creation code here
+    TextFieldWithBrowseButton textFieldWithBrowseButton = new TextFieldWithBrowseButton();
+    textFieldWithBrowseButton.addActionListener(new ComponentWithBrowseButton.BrowseFolderActionListener<>(ExecutionBundle.message("select.working.directory.message"), null,
+      textFieldWithBrowseButton,
+      Arrays.stream(ProjectManager.getInstance().getOpenProjects()).findFirst().orElse(
+        ProjectManager.getInstance().getDefaultProject()
+      ),
+      FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+      TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT) {
+      @Override
+      protected void onFileChosen(@NotNull VirtualFile chosenFile) {
+        super.onFileChosen(chosenFile);
+        if(pluginSettingsModel == null) return;
+
+        pluginSettingsModel.setCustomAssetsPath(textFieldWithBrowseButton.getText());
+        populateDirectory(textFieldWithBrowseButton.getText());
+      }
+    });
+    this.selectDir = LabeledComponent.create(textFieldWithBrowseButton,
+      ExecutionBundle.message("run.configuration.working.directory.label"));
   }
 }
