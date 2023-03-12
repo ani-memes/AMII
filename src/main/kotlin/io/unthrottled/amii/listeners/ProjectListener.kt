0a997a9d69cc4a8dda4869f2dc6129ad25b962e7
Alex Simons <alexsimons9999@gmail.com>
2023-03-12 12:52:57 -0500

    Fixed project open listener.ˆ

--- src/main/kotlin/io/unthrottled/amii/listeners/ProjectListener.kt
+++ src/main/kotlin/io/unthrottled/amii/listeners/ProjectListener.kt
@@ -2,15 +2,19 @@ package io.unthrottled.amii.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.StartupActivity
 import io.unthrottled.amii.PluginMaster
 import io.unthrottled.amii.tools.Logging
 
-internal class ProjectListener :
-  ProjectManagerListener, Logging {
-
-  override fun projectOpened(project: Project) {
+internal class PluginPostStartUpActivity : StartupActivity {
+  override fun runActivity(project: Project) {
     PluginMaster.instance.projectOpened(project)
   }
+}
+
+
+internal class ProjectListener :
+  ProjectManagerListener, Logging {
 
   override fun projectClosed(project: Project) {
     PluginMaster.instance.projectClosed(project)
